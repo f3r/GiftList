01812233d31b039f0fd12bd2e0b9f5d1b3a30cb6
Dominic Ho <domtkho@gmail.com>
2014-11-17 17:03:28 +0800

    partly configuratred omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, '712958552121555', '44d8ffa9305107b0d40cd2d5037552b1', scope: 'user,public_repo'
+  config.omniauth :facebook, '712958552121555', '44d8ffa9305107b0d40cd2d5037552b1'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # devise_scope :user do
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 
   # Example resource route with options:
   #   resources :products do
