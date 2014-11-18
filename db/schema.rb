b4e61b65606f0823c7b84d12ed0430172c19df10
Dominic Ho <domtkho@gmail.com>
2014-11-18 15:33:46 +0800

    Switched to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141117114109) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: true do |t|
     t.text     "content"
     t.integer  "user_id"
@@ -57,8 +60,8 @@ ActiveRecord::Schema.define(version: 20141117114109) do
     t.string   "image"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "wanted_items", force: true do |t|
     t.integer  "user_id"
