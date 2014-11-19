c9e4eea343af5d0d23f353844208326798c216bd
Dominic Ho <domtkho@gmail.com>
2014-11-19 15:37:38 +0800

    Fixed minor sign out bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @items = Item.all
-    @lists = current_user.lists.all
+    if user_signed_in?
+      @lists = current_user.lists.all
+    end
   end
 end
