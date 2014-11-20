ce5bcf4cfd5c15cf6d7c3f73cdcefbedd9ed00e7
Stephen <stephen.parker@timehascome.org>
2014-11-20 14:06:23 +0800

    rudimentary search

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,11 @@ class ItemsController < ApplicationController
   # GET /items.json
   def index
     @items = Item.all
+    if params[:search]
+      @items = Item.search(params[:search]).order("created_at DESC")
+    else
+      @items = Item.all.order('created_at DESC')
+    end
   end
 
   # GET /items/1
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
   has_many  :wanted_items
+def self.search(query)
+  where("name like ?", "%#{query}%") 
+end
 end
